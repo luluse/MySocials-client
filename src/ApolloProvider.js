cdd4c9da2622af51075659ae1df920ad0e0048ce
LULU SEVIGNON <LUDIVINE.SEVIGNON@GMAIL.COM>
2021-04-22 12:11:49 -0700

    init

---
+++ src/ApolloProvider.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import App from './App';
+import { InMemoryCache, ApolloClient, HttpLink } from '@apollo/client';
+import { ApolloProvider } from '@apollo/react-hooks';
+import { setContext } from 'apollo-link-context';
+
+const httpLink = new HttpLink({
+  uri: 'http://localhost:5000'
+});
+
+const authLink = setContext(() => {
+  const token = localStorage.getItem('jwtToken');
+  return {
+    headers: {
+      Authorization: token ? `Bearer ${token}` : ''
+    }
+  };
+});
+
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+});
+
+export default (
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+);
