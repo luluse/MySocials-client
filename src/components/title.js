5bb92cffe7c880b6c016a0a6405983ac77c80d4a
LULU SEVIGNON <LUDIVINE.SEVIGNON@GMAIL.COM>
2021-04-27 09:48:08 -0700

    added logo

--- src/components/title.js
+++ src/components/title.js
@@ -4,8 +4,8 @@ function Title(){
   return(
    <h1 className="ui center aligned header pink">
    
-   <img className="ui image" src="https://i.ibb.co/M2bpZgk/avatar-default.jpg" alt=""/>
-  <div class="content">
+   <img className="ui image" src="https://i.ibb.co/frHVqF8/mysocialslogo.png" alt=""/>
+  <div className="content">
     MySocials
   </div>
   </h1>
