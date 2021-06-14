2de38ce1fe5413a768e18bc3ecff3e29379034ad
LULU SEVIGNON <LUDIVINE.SEVIGNON@GMAIL.COM>
2021-06-14 10:18:38 -0700

    fixed boken image

--- src/pages/SinglePost.js
+++ src/pages/SinglePost.js
@@ -60,7 +60,7 @@ function SinglePost(props) {
         <Grid.Row>
           <Grid.Column width={2}>
             <Image className="ui circular mini right floated image"
-              src="https://i.ibb.co/M2bpZgk/avatar-default.jpg"
+              src="https://lh3.googleusercontent.com/o4U2kkoTg_wNdljf6X84hB6I4ObY4gdadPL6ZxlvfXkn_jE96JwqDRgl8n_XhwOnp41_m28EH95gqE3eKonaARhHuaixPRpcv-34niqXhZU49Q-_7wBI-ecpAG9RPD3dhCTDsNVJOqTpyAR1--RF03rFFc_NTguUkqEmHXSt1JzwTzeTMtFdmS8QuNhR6kMFYJWZhC2cN8HFKM3wC9yEEka6E1PnrQpyM-zKuGkPGegEN96YMBV1Ne0d89q_fMJhCJwCICXS5RHI7u35qH_28tR9VsIha7YIkQ8yqSShKs5SBjdGqnLTrYIGkE_CqHohbya5jpZCwu8OybCFaVTT-9KHfKFL5j7I-QJjDSJ5Q4_VZQ1S6_k8Pzbr5BkOSjIjq41vWt0Os3mDU12NTyROf4hEcbp0Q5UOAG5BndGlWdLAi9EgOU5i2aEd28W9SOINovI7BgpErqZkRyBo2hCKc1zes4Gt__lSdWmnVC_v4-vggNGPu7X4hJgm1Qv_FbJS6ub-dagIkNc6yrHjCbaQdQHJTO-5Uqv_tjYzGo977kEQwQ3INICChFB7tTrrDQXOvst3VmeIRwaAbi5GykH4lxHvxMtPEy8cElq6cwhfj4xm9X6oYSHyRb1fTYHljKxok42i4fczDxp140Ld-DlGrhL-Ub2xj01fwqxdFeAzBefFS7bwRjrrJUqvSI4bOfhEWw-3tItufUv7x8YnydUV567iKw=s132-no?authuser=0"
 
             />
           </Grid.Column>
