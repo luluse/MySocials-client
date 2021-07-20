3b730b91bb3584dc7692acc30c8fb5c708e5f30b
LULU SEVIGNON <LUDIVINE.SEVIGNON@GMAIL.COM>
2021-07-20 14:57:36 -0700

    fixed logo and avatar image links

--- src/components/title.js
+++ src/components/title.js
@@ -4,7 +4,7 @@ function Title(){
   return(
    <h1 className="ui center aligned header pink">
    
-   <img className="ui image" src="https://lh3.googleusercontent.com/vDGpEyX0DoQtEMnwaw13FYso-gvBJS9w6K0ypsp_jR7suxxSLfm03p79IDytvgj9IxFrpAUdI5lKcn8sWabDb7z6wvnAdcqNLW007uz4hmAYByRMtAVHluYBO4Eb7w2SwS9VnEjUz3LiYr6jjKbkOuB-94Bn_I-kHw-Y0IAdfIZajGb8MF0_IcnvDtqfCGBaIdMggx8xKW5femj6B7DucgagJk48ZvtNkcp5xfOlRgotcWyRvdRTaMYcE3t0WRCNxtC6ao5kOjJnK6YG3RJtQ5MKKg8k5RQAGVCltq_lc2wvXZLxqB0665REAdivTa-beh1_qrLv4gceehxFS4qA9Mej-0Ykn-MkU5_I9jWo5KYC0bH487obUCYBUkqIbgFkS1atZ6O-wi4J3o4WY9gLPVJbdYnS_rB7SkzNqDxwjs5BqRuAFZGzlM4OWBdWE_9pGybGWpH9beo2hTzb1zzru0Xpgaf-hT-pGrxakdbnNcgJh4DjWgwwBZNjpypT81h0gno4d5TYatuk5BZ7rsZxXFniXDj4qu4s1CF5RcY2RpFg5b7wD4bwCE07qJ689RdA60kW8ermjLnQlv5NdSXTEiRJHJTcY1_V34XnL3wjYnhzjlu2Dr2s26F_wNpOOi3WB-iP2pf3qHxxxW1jz6NAFWpASSLZbCEsY0FledQ3mM_wUv_HaObEUT3WPFoO9t2Kr40VOVsg3MxOD63I9S-B6A_IIA=w689-h365-no?authuser=0" alt=""/>
+   <img className="ui image" src="https://i.ibb.co/LdMgg5s/mysocials.png" alt=""/>
   <div className="content">
     MySocials
   </div>
